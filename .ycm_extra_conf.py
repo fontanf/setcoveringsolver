7cd8d0859980d66472111929f2b443ec6b00f6ce
Florian Fontan <dev@florian-fontan.fr>
2020-12-27 11:04:46 +0100

    Update .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,6 +6,7 @@ def Settings( **kwargs ):
                 '-I', '.',
                 '-I', './bazel-setcoveringsolver/external/json/single_include',
                 '-I', './bazel-setcoveringsolver/external/googletest/googletest/include',
+                '-I', './bazel-setcoveringsolver/external/boost/',
                 '-I', './bazel-setcoveringsolver/external/optimizationtools',
                 '-I', '/home/florian/Programmes/gurobi811/linux64/include/',
                 ],
